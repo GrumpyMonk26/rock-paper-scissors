b603089c3014b798c1e7183f40f9b7c8328b607c
Jason Evans <evans.jason@hotmail.co.uk>
2023-05-27 20:48:20 +0100

    refactored js code, add comments

--- script.js
+++ script.js
@@ -1,41 +1,56 @@
-const buttons = document.querySelectorAll('button');
-
+// Select all game buttons
+const gameButtons = document.querySelectorAll('button');
+// Select result element
 const resultEl = document.getElementById('result');
-
+// Select player score element
 const playerScoreEl = document.getElementById('user-score');
-
+// Select computer score element
 const computerScoreEl = document.getElementById('computer-score');
 
+// Initialize player and computer scores
 let playerScore = 0;
 let computerScore = 0;
 
-buttons.forEach((button) => {
+// Add click event listener to each game button
+gameButtons.forEach((button) => {
   button.addEventListener('click', () => {
+    // Play a round when a button is clicked
     const result = playRound(button.id, computerPlay());
+    // Update the result text
     resultEl.textContent = result;
   });
 });
 
+// Generate computer's choice
 function computerPlay() {
   const choices = ['rock', 'paper', 'scissors'];
-  const randomChoice = Math.floor(Math.random() * choices.length);
-  return choices[randomChoice];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
 }
 
+// Play a round of the game
 function playRound(playerSelection, computerSelection) {
+  // Define winning combinations
+  const winningCombos = {
+    rock: 'scissors',
+    paper: 'rock',
+    scissors: 'paper',
+  };
+
+  // Check for a tie
   if (playerSelection === computerSelection) {
     return "It's a tie!";
-  } else if (
-    (playerSelection === 'rock' && computerSelection === 'scissors') ||
-    (playerSelection === 'paper' && computerSelection === 'rock') ||
-    (playerSelection === 'scissors' && computerSelection === 'paper')
-  ) {
+  }
+  // Check if the player wins
+  else if (winningCombos[playerSelection] === computerSelection) {
     playerScore++;
     playerScoreEl.textContent = playerScore;
-    return 'You win! ' + playerSelection + ' beats ' + computerSelection;
-  } else {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  }
+  // The computer wins
+  else {
     computerScore++;
     computerScoreEl.textContent = computerScore;
-    return 'You lose! ' + computerSelection + ' beats ' + playerSelection;
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
   }
 }
